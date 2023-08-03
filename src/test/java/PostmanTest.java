9f9c15d67a8a2a414fefc0fb647da90ed06acde8
Katrin <e_aksenova80@mail.ru>
2023-08-03 15:02:31 +0300

    PostmanTest.java added

--- src/test/java/PostmanTest.java
+++ src/test/java/PostmanTest.java
@@ -16,10 +16,27 @@ public class PostmanTest {
 // Выполняемые действия
                 .when()
                 .post("/post")
-// Проверки
+// Проверки then//
                 .then()
                 .statusCode(200)
                 .body("data", equalTo("some data"));
+
+    }
+
+    @Test
+    void shouldPostmanSample() {
+        // Given - When - Then
+// Предусловия
+        given()
+                .baseUri("https://postman-echo.com")
+                .body("some value") // отправляемые данные (заголовки и query можно выставлять аналогично)
+// Выполняемые действия
+                .when()
+                .post("/post")
+// Проверки then//
+                .then()
+                .statusCode(200)
+                .body("data", equalTo("some value"));
 
     }
 }
